bb68f606fc071c422be47d927ea1430cbb7f0c1c
John Wesley <john.silva@dcx.ufpb.br>
2025-02-22 02:27:15 -0300

    Final DijkstraSP, Final DirectedEdge and first page EdgeWeightDiagraph

--- src/DijkstraSP.java
+++ src/DijkstraSP.java
@@ -54,8 +54,75 @@ public class DijkstraSP {
     }
     public Iterable<DirectedEdge> pathTo(int v){
         validateVertex(v);
-        if(!hasPathto())
+        if(!hasPathto(v)) return null;
+        Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+        for(DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]){
+            path.push(e);
+        }
+        return path;
     }
+    private boolean check(EdgeWeightDiagraph G, int s){
+        for(DirectedEdge e : G.edges()){
+            if(e.weight()<0){
+                System.err.println("negative edge weight detected");
+                return false;
+            }
+        }
+        if(distTo[s] != 0.0 || edgeTo[s] != null){
+            System.err.println("distTo[s] and edgeTo[s] inconsistent");
+            return false;
+        }
+        for(int v = 0; v<G.V(); v++){
+            if(v == s) continue;
+            if(edgeTo[v] == null && distTo[v] != Double.POSITIVE_INFINITY){
+                System.err.println("distTo[] and edgeTo[] inconsistent");
+                return false;
+            }
+        }
+        for(int v = 0; v < G.V(); v++){
+            for(DirectedEdge e : G.adj(v)){
+                int w = e.to();
+                if(distTo[v]+e.weight()<distTo[w]){
+                    System.err.println("edge "+ e +" not relaxed");
+                    return false;
+                }
+            }
+        }
+        for(int w = 0; w < G.V(); w++){
+            if(edgeTo[w] == null) continue;
+            DirectedEdge e = edgeTo[w];
+            int v = e.from();
+            if(w!= e.to()) return false;
+            if(distTo[v] + e.weight()!= distTo[w]){
+                System.err.println("edge "+ e +" on shortest path not tight");
+                return false;
+            }
+        }
+        return true;
+    }
+    private void validateVertex(int v){
+        int V = distTo.length;
+        if(v<0 || v>= V){
+            throw new IllegalArgumentException("vertex "+ v + " is not between 0 and " + (V-1));
+        }
+    }
+    public static void main(String [] args){
+        In in = new In(args[0]);
+        EdgeWeightDiagraph G = new EdgeWeightedDiagraph(in);
+        int s = Integer.parseInt(args[1]);
 
-    // pagina 3 Interable das imagens
+        DijkstraSP sp = new DijkstraSP(G, s);
+        for (int t = 0; t < G.V(); t++){
+            if(sp.hasPathto(t)){
+                StdOut.printf("%d to %d (%.2f) ", s, t, sp.distTo(t));
+                for(DirectedEdge e : sp.pathTo(t)){
+                    StdOut.print(e + "  ");
+                }
+                StdOut.println();
+            } else{
+                StdOut.printf("%d to %d                 nopath\n", s, t);
+            }
+
+        }
+    }
 }
